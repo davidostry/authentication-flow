6facbe5e9b0020849600d1610b9ba931e9c4a11c
david <sarigoo421@gmail.com.il>
2026-09-16 15:21:37 +0300

    added zustand

---
+++ fronted/src/store/authStore.ts
@@ -0,0 +1,36 @@
+import { create } from 'zustand';
+
+type AuthStore = {
+    token: string | null;
+    setToken: (token: string) => void;
+    logout: () => void;
+};
+
+export const useAuthStore = create<AuthStore>((set) => ({
+    token: localStorage.getItem("token"),
+
+    setToken: (token) => {
+        localStorage.setItem("token", token);
+        set({ token });
+    },
+    logout: () => {
+        localStorage.removeItem("token");
+        set({ token: null });
+    },
+}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
